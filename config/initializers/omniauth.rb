be7356e960ffb5254e0136362dd11a99be776c26
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-07-29 11:04:53 -0600

    completed the client secret and client id now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, Rails.application.credentials.development[:google_id], Rails.application.credentials.development[:google_secret]
 end
 OmniAuth.config.allowed_request_methods = %i[get]
