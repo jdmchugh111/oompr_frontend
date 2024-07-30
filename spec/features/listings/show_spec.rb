06b5c3822cc5f56f194106dddd55a6493a4a20d3
Zach Bergman <bzach634@gmail.com>
2024-07-29 21:54:08 -0600

    commit

--- spec/features/listings/show_spec.rb
+++ spec/features/listings/show_spec.rb
@@ -43,18 +43,67 @@ RSpec.describe "Listing Show Page" do
       end
     end
 
-    it "I should see a 'Reality Check' button", :vcr do
-      visit listing_path(1005254)
-
-      within('.rc-button') do
-        expect(page).to have_button("Reality Check")
-      end
-    end
-
     it "there is a heart icon that I can click to save the listing to favorites (if signed in)" do      
     end
 
     it "if not signed in, clicking the heart prompts me to create an account or sign in" do
     end
+
+    describe "Reality Check" do
+      it "I should see a 'Reality Check' button", :vcr do
+        visit listing_path(1005254)
+
+        within('.rc-button') do
+          expect(page).to have_button("Reality Check")
+        end
+      end
+
+      it "clicking the 'Reality Check' button shows a number field and prompts me to enter my monthly income,
+      when the submit button is clicked, a modal should pop up and show what I make, versus what I would need to make", :vcr do
+        visit listing_path(1005254)
+
+        click_button "Reality Check"
+
+        expect(page).to have_content("Enter your monthly income:")
+
+        fill_in :monthly, with: 375000
+
+        click_button "Submit"
+
+        expect(page).to have_css('.modal', visible: true)
+
+        within('.modal-body') do
+          expect(page).to have_content("What You Make:")
+          expect(page).to have_content("What You Would Need To Make:")
+        end
+      end
+
+      it "if I have previously entered my monthly income, that number should be stored in a cookie", :vcr do
+        visit listing_path(1005254)
+
+        click_button "Reality Check"
+
+        fill_in :monthly, with: 375000
+
+        click_button "Submit"
+
+        visit listing_path(1005254)
+
+        click_button "Reality Check"
+
+        expect(find_field(:monthly).value).to eq('375000')
+      end
+    end
   end
 end
+
+# As a user, if I'm on a listing show page
+# I should see a "Reality Check" button
+# When I click it, a number field appears and I'm prompted to enter my monthly income
+# (Unless I have previously entered my monthly income, in which case that number should be stored in a cookie)
+# Once submitted, the Reality Check should display:
+# "What You Make: $XXX"
+# "What You Would Need To Make: $XXX"
+
+# maybe just make modal that pops up to tell prices instead of a whole new page?
+# how to store the monthly income in a cookie using the modal, since not going to a controller?
